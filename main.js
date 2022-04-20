8848cc080606cf5f861e47b8056990f7ec57069a
Xavier <47183833+Xavier-Corbier@users.noreply.github.com>
2022-04-20 14:09:48 +0200

    update main

---
+++ main.js
@@ -0,0 +1,5 @@
+// Reads the number of disks in the puzzle
+const numberOfDisks = parseInt(readline());
+
+// Moves the smallest disk from the source-tower 0 (left) to the target-tower 1 (middle)
+console.log('move 0 1');
