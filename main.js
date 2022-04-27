852e1ec832d6db47e19767f6a4ad70e5d1be83df
Xavier <47183833+Xavier-Corbier@users.noreply.github.com>
2022-04-27 13:22:47 +0200

    update game

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+// -------------------------------
+// Fichier généré automatiquement par polyconquest :
+// Veuillez utiliser ce fichier pour envoyer votre code
+// -------------------------------
+
 // Reads the number of disks in the puzzle
 const numberOfDisks = parseInt(readline());
 
-// Moves the smallest disk from the source-tower 0 (left) to the target-tower 1 (middle)
-console.log('move 0 1');
-console.log('move 0 2');
+
+console.log("move 0 1")
+console.log("move 0 2")
